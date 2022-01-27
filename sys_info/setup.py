91df300097d08b700fe1284883d8e60dc371b1a2
21205925 <93437756+21205925@users.noreply.github.com>
2022-01-27 09:27:56 +0000

    new folder with everything in it

---
+++ sys_info/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='systeminfo',
+      version='0.1',
+      description='Systems infos on display',
+      url='https://github.com/21205925/comp30830',
+      author='Elise BRARD',
+      author_email='elise.brard@ucdconnect.ie',
+      license='UCD',
+      packages=['systeminfo'],
+      zip_safe=False)
