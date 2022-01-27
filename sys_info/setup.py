9ae4a705fa48e1b368f5eeead43dae53a84b1e0e
21205925 <93437756+21205925@users.noreply.github.com>
2022-01-27 09:48:04 +0000

    test sur mon ordi

--- sys_info/setup.py
+++ sys_info/setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(name='sys_info',
       version='0.1',
       description='Systems infos on display',
-      url='https://github.com/21205925/comp30830',
+      url=' C:\Users\elisebrard\Documents\GitHub\comp30830\sys_info',
       author='Elise BRARD',
       author_email='elise.brard@ucdconnect.ie',
       license='UCD',
       packages=['sys_info'],
+      install_requires = ['psutil'],
       zip_safe=False)
